396b28f9edbd70420c66cf60cfa32d959e1c2ccc
Mohamed Arif <arifmohamed.techzar@gmail.com>
2023-10-31 23:54:26 +0530

    template setup proccess

---
+++ resources/views/admin/layout/script.blade.php
@@ -0,0 +1,15 @@
+<script src="{{ asset('assets/template/assets/vendors/js/vendor.bundle.base.js')}}"></script>
+<!-- endinject -->
+<!-- Plugin js for this page -->
+<script src="{{ asset('assets/template/assets/vendors/chart.js/Chart.min.js')}}"></script>
+<script src="{{ asset('assets/template/assets/js/jquery.cookie.js')}}" type="text/javascript"></script>
+<!-- End plugin js for this page -->
+<!-- inject:js -->
+<script src="{{ asset('assets/template/assets/js/off-canvas.js')}}"></script>
+<script src="{{ asset('assets/template/assets/js/hoverable-collapse.js')}}"></script>
+<script src="{{ asset('assets/template/assets/js/misc.js')}}"></script>
+<!-- endinject -->
+<!-- Custom js for this page -->
+<script src="{{ asset('assets/template/assets/js/dashboard.js')}}"></script>
+<script src="{{ asset('assets/template/assets/js/todolist.js')}}"></script>
+<!-- End custom js for this page -->
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,12 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('auth.login');
 });
+Route::get('/template', function () {
+    return view('template');
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
